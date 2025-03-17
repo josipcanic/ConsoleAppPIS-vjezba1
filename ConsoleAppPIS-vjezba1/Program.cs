d52da23ecf0630e1b71524393ed7dba36fe60bee
josipcanic <146947488+josipcanic@users.noreply.github.com>
2025-03-17 17:42:09 +0100

    Update Program.cs

--- ConsoleAppPIS-vjezba1/Program.cs
+++ ConsoleAppPIS-vjezba1/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("lele");
-Console.WriteLine("Ja sam najsladi covek na svetu");
-Console.WriteLine("Pretuko bi te ko zvecku");
+
